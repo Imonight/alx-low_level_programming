ede697178cabd730bb4403aafbc2ee9ed8dc9054
Imonight <imonight1@gmail.com>
2022-09-08 07:18:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,11 +5,10 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */ 
-
+ */
 int main(void)
 {
 	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+		write(2, ch, sizeof(ch));
 	return (1);
 }
