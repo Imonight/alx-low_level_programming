9da367d924d59fcc776e616c4835e3d1e4e71e63
Imonight <imonight1@gmail.com>
2022-09-08 07:24:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-		write(2, ch, sizeof(ch));
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
 	return (1);
 }
