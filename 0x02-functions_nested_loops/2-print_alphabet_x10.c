b4619707eda4c44757eb1f70b965f52d3a19968a
Imonight <imonight1@gmail.com>
2022-09-13 12:57:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_alphabet_x10(void) - print abc x10
+ *
+ * Return: 0 always (success)
+ */
+void print_alphabet_x10(void)
+{
+	char c, i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		putchar('\n');
+
+	}
+}
