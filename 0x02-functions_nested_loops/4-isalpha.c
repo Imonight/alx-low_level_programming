175dc300c50bd25e138df76bfa4099c932740f61
Imonight <imonight1@gmail.com>
2022-09-13 14:16:57 -0700

    _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha to check if is a letter
+ *@C: is the int use for the argument
+ *Return: 0
+ */
+int _isalpha(int C)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
+
