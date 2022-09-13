b0dbdfa8abd3b732779728c4b3a02ad79c7089d7
Imonight <imonight1@gmail.com>
2022-09-13 16:45:30 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,18 @@
 /**
  *print_last_digit - prints the last digit only
  *@n: the number in question
- *Return:
+ *Return: the value of the last digit
  */
+
 int print_last_digit(int n)
 {
+
 	int last_digit = n % 10;
+
 	if (last_digit < 0)
 	last_digit *= -1;
+	
 	_putchar(last_digit + '0');
+	
 	return (last_digit);
 }
