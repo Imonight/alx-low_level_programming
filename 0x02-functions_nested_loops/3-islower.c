c3c215d0ac4c3aae0737e97f7348c685787a598f
Imonight <imonight1@gmail.com>
2022-09-13 13:28:11 -0700

    print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * islower - function to check lowercase character
+ *
+ * Return: 0 always (success)
+ */
+int _islower(int c)
+{
+	if (c >= 'a'; && c <= 'z')
+	{
+		return(1);
+	}
+	else
+		return (0);
+}	
