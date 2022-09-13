acf7a7a529226762449dbad5a1455ad6dfab23d5
Imonight <imonight1@gmail.com>
2022-09-13 15:04:39 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,33 @@
-#include <stdio.h>
+#include <limits.h>
+#include "main.h"
 /**
- *print_sign - function to check the sign of a number
- *@n is the int we use for the argument of the function
- *Return: 0
- */
-int print_sign(int n)
-{
-	if (n > 0)
-	{
-	putchar('+');
-	return (1);
-	}
-	else if (n < 0)
-	{
-	putchar('-');
-	return (-1);
-	}
-	else
-	{
-	putchar('0');
-	return (0);
-	}
-}
+ *
+ *  * main - check the code for Holberton School students.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
 
+int main(void)
+
+{
+
+		int r;
+
+
+
+			r = print_sign(98);
+
+				if (r < 0)
+
+							r *= -1;
+
+					_putchar(r + '0');
+
+						_putchar('\n');
+
+							return (0);
+
+}
