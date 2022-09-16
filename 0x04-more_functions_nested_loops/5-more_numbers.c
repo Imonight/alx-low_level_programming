ac372a3dfa0bb53e81f440ac012dd90e7b3f885f
Imonight <imonight1@gmail.com>
2022-09-16 02:32:42 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,19 @@ void more_numbers(void)
 	char n, c;
 	int i = 0;
 
-	for (n = 0; n <= 14; n++)
-	{
 	while (i < 10)
 	{
-		c = n;
-		if (n > 9)
+		for (n = 0; n <= 14; n++)
 		{
-			_putchar('1');
-			c = n % 10;
+			c = n;
+			if (n > 9)
+			{
+				_putchar('1');
+				c = n % 10;
+			}
+			_putchar('0' + c);
 		}
-		_putchar('0' + c);
-	}
-	_putchar('\n');
-	i++;
+		_putchar('\n');
+		i++;
 	}
 }
