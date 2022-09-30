fbcc12384e6301161f4f9f76583be69db3702b11
Imonight <imonight1@gmail.com>
2022-09-30 14:15:24 -0700

    good

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+#include<stdio.h>
+#define UNUSED(x) (void)(x)
 /**
  * main - Prints the name of the program
  * @argc: Number of command line arguments
  * @argv: Array name
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[argc * 0]);
+	UNUSED(argc);
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
